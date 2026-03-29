8c598b37fb2e7b7c7321722c5d717d06242ee2f1
Christian-39 <ezechristian1999@gmail.com>
2026-03-29 16:12:08 +0100

    new

--- backend/tobaz_autos/settings.py
+++ backend/tobaz_autos/settings.py
@@ -21,7 +21,7 @@ DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/tobaz_autos/urls.py
+++ backend/tobaz_autos/urls.py
@@ -6,6 +6,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Tobaz Autos API is running", "status": "online"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
